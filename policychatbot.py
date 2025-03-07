428ca3b014e167e064581c1fc714102dfff7b4fd
KalyshaUtama <156767748+KalyshaUtama@users.noreply.github.com>
2025-03-07 22:27:03 +0300

    Update policychatbot.py

--- policychatbot.py
+++ policychatbot.py
@@ -23,21 +23,25 @@ def add_document(doc):
   print(text)
   chunk_size = 512
   chunks = [text[i : i + chunk_size] for i in range(0, len(text), chunk_size)]
-  client = Mistral(api_key=api_key)
-  text_embeddings = client.embeddings.create(model="mistral-embed",
-  inputs=list_txt_chunks).data
+  text_embeddings = get_text_embedding(chunks)
   embeddings = np.array([text_embeddings[i].embedding for i in range(len(text_embeddings))])
   d = len(text_embeddings[0].embedding)
   index = faiss.IndexFlatL2(d)
   index.add(embeddings)
+
+def get_text_embedding(list_txt_chunks):
+  client = Mistral(api_key=api_key)
+  embeddings_batch_response = client.embeddings.create(model="mistral-embed",
+  inputs=list_txt_chunks)
+  return embeddings_batch_response.data
 
 st.title("UDST policy chatbot")
 
 option = st.selectbox(
     "Which policy would you like to inquire about?",
     (
-"Academic Appraisal Policy -V1	PL-AC-09",
-"Academic Appraisal Procedure-V1 	PR-AC-09",
+"academic-annual-leave-policy",
+"academic-appraisal-policy",
 "Academic Credentials Policy - V1 	PL-AC-02",
 "Academic Freedom Policy- V1 ",
 "Academic Members' Retention Policy- V1 	PL-AC-12",
@@ -49,6 +53,38 @@ option = st.selectbox(
 )
 
 st.write("You selected:", option)
+add_document(f"https://www.udst.edu.qa/about-udst/institutional-excellence-ie/policies-and-procedures/{option}")
+
+query = st.text_input("Enter your query:")
+question_embeddings = np.array([get_text_embedding([query])[0].embedding])
+D, I = index.search(question_embeddings, k=2)
+retrieved_chunk = [chunks[i] for i in I.tolist()[0]]
+
+prompt = f"""
+Context information is below.
+---------------------
+{retrieved_chunk}
+---------------------
+Given the context information and not prior knowledge, answer the query.
+Query: {question}
+Answer:
+"""
+
+def mistral(user_message, model="mistral-small-latest", is_json=False):
+  model = "mistral-large-latest"
+  client = Mistral(api_key=api_key)
+  messages = [
+  UserMessage(content=user_message),
+  ]
+  chat_response = client.chat.complete(
+  model=model,
+  messages=messages,
+  )
+  )
+  return chat_response.choices[0].message.content
+
+response = mistral(prompt)
+print(response)
 
 
           

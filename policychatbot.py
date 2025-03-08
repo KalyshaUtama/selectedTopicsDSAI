a7f4e797f4c6e26e8c3452a59f6e24411976cb57
KalyshaUtama <156767748+KalyshaUtama@users.noreply.github.com>
2025-03-08 21:47:59 +0300

    Update policychatbot.py

--- policychatbot.py
+++ policychatbot.py
@@ -11,7 +11,7 @@ import numpy as np
 os.environ["MISTRAL_API_KEY"] = "b1HKlTtc2a1pqPEgMcqm8sVqckYAdfMj"
 print(f"MISTRAL_API_KEY: {os.environ.get('MISTRAL_API_KEY')}")
 api_key = os.getenv("MISTRAL_API_KEY")
-
+@st.cache_data(ttl=86400)  
 def get_text_embedding(list_txt_chunks):
   client = Mistral(api_key=api_key)
   embeddings_batch_response = client.embeddings.create(model="mistral-embed",
@@ -52,34 +52,35 @@ index = faiss.IndexFlatL2(d)
 index.add(embeddings)
 
 query = st.text_input("Enter your query:")
-question_embeddings = np.array([get_text_embedding([query])[0].embedding])
-D, I = index.search(question_embeddings, k=2)
-retrieved_chunk = [chunks[i] for i in I.tolist()[0]]
-
-prompt = f"""
-Context information is below.
----------------------
-{retrieved_chunk}
----------------------
-Given the context information and not prior knowledge, answer the query.
-Query: {query}
-Answer:
-"""
-
-def mistral(user_message, model="mistral-small-latest", is_json=False):
-  model = "mistral-large-latest"
-  client = Mistral(api_key=api_key)
-  messages = [
-  UserMessage(content=user_message),
-  ]
-  chat_response = client.chat.complete(
-  model=model,
-  messages=messages,
-  )
-  return chat_response.choices[0].message.content
-
-response = mistral(prompt)
-st.write(response)
+if query:
+  question_embeddings = np.array([get_text_embedding([query])[0].embedding])
+  D, I = index.search(question_embeddings, k=2)
+  retrieved_chunk = [chunks[i] for i in I.tolist()[0]]
+  
+  prompt = f"""
+  Context information is below.
+  ---------------------
+  {retrieved_chunk}
+  ---------------------
+  Given the context information and not prior knowledge, answer the query.
+  Query: {query}
+  Answer:
+  """
+  
+  def mistral(user_message, model="mistral-small-latest", is_json=False):
+    model = "mistral-large-latest"
+    client = Mistral(api_key=api_key)
+    messages = [
+    UserMessage(content=user_message),
+    ]
+    chat_response = client.chat.complete(
+    model=model,
+    messages=messages,
+    )
+    return chat_response.choices[0].message.content
+  
+  response = mistral(prompt)
+  st.write(response)
 
 
           

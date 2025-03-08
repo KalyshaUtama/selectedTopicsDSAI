b67015a2ff7cc776c182a7a9c4ca93678acfb99b
KalyshaUtama <156767748+KalyshaUtama@users.noreply.github.com>
2025-03-08 22:32:57 +0300

    Update mistral.py

--- mistral.py
+++ mistral.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import numpy as np
 import os
+from mistralai import Mistral, UserMessage
 
 os.environ["MISTRAL_API_KEY"] = "HVu1lheglNRREvb4XO5Yvm7GrcsufpLj"
 print(f"MISTRAL_API_KEY: {os.environ.get('MISTRAL_API_KEY')}")
 api_key = os.getenv("MISTRAL_API_KEY")
 
-import os
-from mistralai import Mistral, UserMessage
+
 def mistral(user_message, model="mistral-small-latest", is_json=False):
     model = "mistral-large-latest"
     client = Mistral(api_key=api_key)
@@ -24,15 +24,9 @@ def mistral(user_message, model="mistral-small-latest", is_json=False):
 # Streamlit UI
 st.title("I am Mistral :))")
 
-option = st.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone"),
-)
-
-st.write("You selected:", option)
 # Input query
 query = st.text_input("Enter your query:")
 
-response = mistral(query)
-
-st.write("Response\n", response)
+if query:
+    response = mistral(query)
+    st.write("Response\n", response)
